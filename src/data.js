b060af4113d80b0fe640271e2a7437af4c1502c5
Reggie <ryoboua@gmail.com>
2020-05-09 19:31:33 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -17,4 +17,28 @@ export default [
     image:
       "https://s3-alpha-sig.figma.com/img/2502/3936/25fed2a85c43f4195dd00d3ee9c95ac6?Expires=1589760000&Signature=d4DpLCvUInhIrqTOrRABppHLwnh4ZFdgHh6pywffCiN-8zu-lFbdJqwHO5wqmAzEhpUThYLD6gkRQStnCQ3KirOKc5rFyrBs~Q939chvl8M6freaNL0pCxCyCO30zBVlkrM5Cm7SnCYgVETdYMbZUkMgtd4dnSJHAoqrqpwUyTdAFwOcF2Yp4T7gRSXyStYzgvFPR3pJzMRReb2sQlV6d94BeZIWWoWwiHDcexdWjjYpOl3jLaEQJyM1Iw5WuLnByIP2rOStCah7q7PfbElPmIg5ot088G79rfxDRyrGbV-RjacJF9HUsYpm9W1aiPIiUz-vRVVifXhU9Mrcg0lBZA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
   },
+  {
+    name: "Man hunt",
+    date: "Fri, 14 December 2020",
+    image:
+      "https://s3-alpha-sig.figma.com/img/2502/3936/25fed2a85c43f4195dd00d3ee9c95ac6?Expires=1589760000&Signature=d4DpLCvUInhIrqTOrRABppHLwnh4ZFdgHh6pywffCiN-8zu-lFbdJqwHO5wqmAzEhpUThYLD6gkRQStnCQ3KirOKc5rFyrBs~Q939chvl8M6freaNL0pCxCyCO30zBVlkrM5Cm7SnCYgVETdYMbZUkMgtd4dnSJHAoqrqpwUyTdAFwOcF2Yp4T7gRSXyStYzgvFPR3pJzMRReb2sQlV6d94BeZIWWoWwiHDcexdWjjYpOl3jLaEQJyM1Iw5WuLnByIP2rOStCah7q7PfbElPmIg5ot088G79rfxDRyrGbV-RjacJF9HUsYpm9W1aiPIiUz-vRVVifXhU9Mrcg0lBZA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+  {
+    name: "Adam the great",
+    date: "Fri, 14 April 2020",
+    image:
+      "https://s3-alpha-sig.figma.com/img/2502/3936/25fed2a85c43f4195dd00d3ee9c95ac6?Expires=1589760000&Signature=d4DpLCvUInhIrqTOrRABppHLwnh4ZFdgHh6pywffCiN-8zu-lFbdJqwHO5wqmAzEhpUThYLD6gkRQStnCQ3KirOKc5rFyrBs~Q939chvl8M6freaNL0pCxCyCO30zBVlkrM5Cm7SnCYgVETdYMbZUkMgtd4dnSJHAoqrqpwUyTdAFwOcF2Yp4T7gRSXyStYzgvFPR3pJzMRReb2sQlV6d94BeZIWWoWwiHDcexdWjjYpOl3jLaEQJyM1Iw5WuLnByIP2rOStCah7q7PfbElPmIg5ot088G79rfxDRyrGbV-RjacJF9HUsYpm9W1aiPIiUz-vRVVifXhU9Mrcg0lBZA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+  {
+    name: "Damn we be all",
+    date: "Sat, 20 June 2020",
+    image:
+      "https://s3-alpha-sig.figma.com/img/795b/8fe4/17ee1ebc86ec4273aac7b51178599116?Expires=1589760000&Signature=Le3x3YuGOZbbMUV4KJPSkjv5ADeQQ0RyIahRJoygA~9gIlx68GWkABj5~HNEo-KIrw3AcnuMQQHQLU5Ba2Y~HB9WyCbUTFNEJ3SbTvxV-qml6TlmDZKFApmdiGoBVvujr~V1kkjtptq~7rqoKjGJZs39yaz7ZIT5jJO2ZvOKrjoVkwApltWajNAQP3eELyQmcjdkVdRYBs0oSo99BgUrKuZKf3C0dvb1nvWs7TK-IMR8q-XPjdrnQKvTuDhMSAeb6CPXnpryqzM8P9yALFKzRLmqaeJkLignEfuCqjJGyKBT6Up3MwKmFaguEoDylKcIbN9obu8dJq3wee2J~HiW9w__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
+  {
+    name: "Victoria won",
+    date: "Sat, 24 May 2020",
+    image:
+      "https://s3-alpha-sig.figma.com/img/b477/81d0/3f9d385e829ff893089fb7ce89d9c81e?Expires=1589760000&Signature=JBMK6y8JMtelyffNpcG4oEPWNarvnMOEaKdzTiKSu1tSrOb0YMBkWVs1~zIkPISMZQ0JUxWN2-lRBlj1QI3QZt6Gt6QRA2JRrwUjbD55OyJFtSLNNLO-7wHqMaBEiEov2W3IV6fVzy0MEYyt9IYeiT83W0k1SCU1BrvNJINNpLoyuLCy5jW3HVvNtDTXt8GTW1b-VT~9QWBx3v70R-k0QrQam4mGKjhrizI6NGGV22AHRAO6rCT8bMmWr5CNYvVTZtmQDexRYwSMdsD8sSPds8yji-m6Fp4i9O24ZpUjKjRLU8u5-2~hzgpyUqpw1UENiLlxGPdHtJgYYqR75WmO7Q__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA",
+  },
 ]
